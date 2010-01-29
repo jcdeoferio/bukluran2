4a60fcad815c92f0b6adfdff70d24d4d373a3a14
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-01-29 20:22:35 +0000

    layout adjustments

--- application/views/content.php
+++ application/views/content.php
@@ -1,6 +1,6 @@
-<div id="content">
+<div id="contentWrapper">
 <div class="container">
-	<div class="span-24 last">
+	<div class="span-24 last" id="content">
 		<H2>Announcements</H2>
 	</div>
 </div>
