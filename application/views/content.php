066a4f52a95337f613fa624eba30a3ca59a8164a
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-02-25 17:08:48 +0000

    incorporated fckeditor for rich text formatting of text fields. a sample editor can be viewed at index.php/main/fckeditorform

--- application/views/content.php
+++ application/views/content.php
@@ -1,7 +1,12 @@
 <div id="contentWrapper">
 <div class="container">
 <div class="span-24 last" id="content">
-	<H2>Announcements</H2>
+	<div class="announcementTitle">
+		<H2>Announcements</H2>
+	</div>
+	<div class="announcementContent">
+	</div>
+	
 </div>
 </div>
 </div>
