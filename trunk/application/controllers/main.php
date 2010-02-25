eb5989e74deb4e04d69720067ead37fef1a3cac5
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-02-25 17:08:48 +0000

    incorporated fckeditor for rich text formatting of text fields. a sample editor can be viewed at index.php/main/fckeditorform

--- trunk/application/controllers/main.php
+++ trunk/application/controllers/main.php
@@ -1,10 +1,30 @@
 <?php
 class Main extends Controller {
-
 	function Main()
 	{
 		parent::Controller();
+		$this->load->helper('url');
 	}
+	
+	function fckeditorform(){
+		$fckeditorConfig = array(
+			'instanceName' => 'content',
+			'BasePath' => base_url().'system/plugins/fckeditor/',
+			'ToolbarSet' => 'Basic',
+			'Width' => '100%',
+			'Height' => '200',
+			'Value' => ''
+			);
+		
+		$this->load->library('fckeditor', $fckeditorConfig);
+		$this->load->view('fckeditorView');
+        
+	}
+	function fckeditorshowpost(){
+    
+        echo $this->input->post('content');
+        
+	} 
 
 	function index()
 	{
