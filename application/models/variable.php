f68cee205b9ab3241187dfdc1d997dbad49f6b4f
cedualino200 <cedualino200@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-05-12 03:17:17 +0000

    Manage Application Period

--- application/models/variable.php
+++ application/models/variable.php
@@ -43,7 +43,12 @@ class Variable extends Model{
 	}
 	
 	function app_is_open(){
-		return($this->get(APP_IS_OPEN));
+		$app_open_state = $this->get(APP_IS_OPEN);
+		
+		if(is_null($app_open_state))
+			log_message('error', 'Illegal state: no APP_IS_OPEN variable entry in variables table');
+		
+		return($app_open_state);
 	}
 	
 	function set_app_open_state($openness){
@@ -54,18 +59,74 @@ class Variable extends Model{
 	}
 	
 	function current_application_aysem(){
-		return($this->get(CURRENT_APP_AYSEM));
+		$current_app_aysem = $this->get(CURRENT_APP_AYSEM);
+		
+		if(is_null($current_app_aysem))
+			log_message('error', 'Illegal state: no CURRENT_APP_AYSEM variable entry in variables table');
+		
+		return($current_app_aysem);
+	}
+	
+	function pretty_current_application_aysem(){
+		$pretty_sem = $this->pretty_current_sem();
+		$pretty_acadyear = $this->pretty_current_acadyear();
+		
+		$pretty_app_aysem = "{$pretty_sem} {$pretty_acadyear}";
+		return($pretty_app_aysem);
+	}
+	
+	function current_sem(){
+		$current_app_aysem = $this->current_application_aysem();
+		
+		$sem = substr($current_app_aysem, 4, 1);
+		
+		return($sem);
+	}
+	
+	function pretty_current_sem(){
+		$sem = $this->current_sem();
+		$pretty_sem = NULL;
+		switch($sem){
+			case 1:
+				$pretty_sem = '1st Semester';
+				break;
+			case 2:
+				$pretty_sem = '2nd Semester';
+				break;
+			case 3:
+				$pretty_sem = 'Summer';
+				break;
+			default:
+				log_message('error', 'Illegal value for sem in CURRENT_APP_AYSEM');
+		}
+		
+		return($pretty_sem);
+	}
+	
+	function current_acadyear(){
+		$current_app_aysem = $this->current_application_aysem();
+		
+		$acadyear = substr($current_app_aysem, 0, 4);
+		
+		return($acadyear);
+	}
+	
+	function pretty_current_acadyear(){		
+		$acadyear = $this->current_acadyear();
+		$pretty_acadyear = "academic year {$acadyear}";
+		
+		return($pretty_acadyear);
 	}
 	
 	function set_current_aysem($aysem){
 		if(!$this->is_legal_aysem($aysem))
-			throw new Exception("Illegal aysem format: '{$aysem}'"); //Change to better exception
+			throw new UnexpectedValueException("Illegal aysem format: '{$aysem}'"); //Change to better exception
 		
 		$this->set(CURRENT_APP_AYSEM, $aysem);
 	}
 	
 	private function is_legal_aysem($aysem){
-		return(preg_match('/^\d{4}[123]$/', $aysem));
+		return(preg_match('/^\d+[123]$/', $aysem));
 	}
 
 }
