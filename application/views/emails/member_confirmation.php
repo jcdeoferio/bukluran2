6016e93bdc2f9b2624e0fe01477b3cd7726f4e98
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-05-26 15:52:48 +0000

    added hashcode to email sent to faculty and students.

--- application/views/emails/member_confirmation.php
+++ application/views/emails/member_confirmation.php
@@ -10,6 +10,10 @@ You can confirm your membership to the organization by going to:<?=br(2)?>
 
 <?=anchor('login/link/'.$member['hashcode'],site_url('login/link/'.$member['hashcode'])).br(2)?>
 
+Your code for logging in is: <b><?= $member['hashcode']?></b><?=br(2)?>
+
+This code is like a password for your account, please refrain from sharing it to others.<?=br(2)?>
+
 If you are not affiliated with <?=$organization['orgname']?>, please ignore this message.<?=br(2)?>
 
 If clicking the link doesn't work, just copy and paste the entire address into your browser. If you're still having problems, simply forward this email to <?=mailto('up.bukluran@gmail.com','up.bukluran@gmail.com')?> and we'll do our best to help you.<?=br(2)?>
--- application/views/login/user_pass.php
+++ application/views/login/user_pass.php
@@ -16,13 +16,16 @@
 			<?= form_input(array('type'=>'text','name'=>'username','id'=>'username','value'=>set_value('username'),'size'=>'23','class'=>'text_input')).br(1)."\n";?>			
 			<?= form_label('Password','password',array('class'=>'label')).br(1)."\n"; ?>
 			<?= form_input(array('type'=>'password','name'=>'password','id'=>'password','size'=>'23','class'=>'text_input')).br(1)."\n";?>
-			
+			<!--
 			<?= form_input(array('type'=>'checkbox','name'=>'rememberme','id'=>'rememberme','value'=>'forever'))."\n";?>
-			<?= form_label('Remember me','rememberme',array('class'=>'label'))."\n"; ?>
 			
+			<?= form_label('Remember me','rememberme',array('class'=>'label'))."\n"; ?>
+			-->
 			<?= form_submit('submit','Log in','class="submit_default" id="submit"')."\n";?>
 			
 		<?= form_close()."\n";?>
+		<!--
 		<?=anchor('login/lost_pass','Lost your password?').br(2)."\n";?>
+		-->
 		For Students and Faculty, click <?=anchor('login/link','here')?>.
 </div>
