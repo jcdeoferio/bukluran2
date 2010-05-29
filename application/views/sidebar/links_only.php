8f7fcfc0e2b89781e8fe1b4496ec47c132714803
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-05-29 12:23:03 +0000

    added extra data for sidebar. highlighting of current page in the sidebar link.

--- application/views/sidebar/links_only.php
+++ application/views/sidebar/links_only.php
@@ -3,7 +3,7 @@
 		<div class='sidebar_links'>
 			<h1><?=$link['title']?></h1>
 			<?php for($i=0;$i<count($link['hrefs']);$i++): ?>
-				<?= anchor($link['hrefs'][$i],$link['anchors'][$i])?>
+				<?= anchor($link['hrefs'][$i],$link['anchors'][$i],($link['selected']==$i?'class="selected"':''))?>
 			<?php endfor;?>
 		</div>
 	<?endforeach;?>
