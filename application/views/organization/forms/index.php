857449cf0d6bad3ee81fd6eb53873639d484b860
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-06-04 03:59:59 +0000

    change_application_status for OSA account.

--- application/views/organization/forms/index.php
+++ application/views/organization/forms/index.php
@@ -11,16 +11,16 @@
 		$progress_form7 = $this->orgregistration_model->form7($this->session->organizationid(),$this->Variable->current_application_aysem());
 		$progress_reqs = $this->orgregistration_model->requirements($this->session->organizationid(),$this->Variable->current_application_aysem());
 	}else{
-		$progress_form1 = $this->orgregistration_model->form1($org['organizationid'],$aysem);
-		$progress_form1_advisers = $this->orgregistration_model->form1_advisers($org['organizationid'],$aysem);
-		$progress_form2 = $this->orgregistration_model->form2($org['organizationid'],$aysem);
-		$progress_form3_members = $this->orgregistration_model->form3_members($org['organizationid'],$aysem);
-		$progress_form3_officers = $this->orgregistration_model->form3_officers($org['organizationid'],$aysem);
-		$progress_form5_eventreports = $this->orgregistration_model->form5_eventreports($org['organizationid'],$aysem);
-		$progress_form5_awards = $this->orgregistration_model->form5_awards($org['organizationid'],$aysem);
-		$progress_form6 = $this->orgregistration_model->form6($org['organizationid'],$aysem);
-		$progress_form7 = $this->orgregistration_model->form7($org['organizationid'],$aysem);
-		$progress_reqs = $this->orgregistration_model->requirements($org['organizationid'],$aysem);
+		$progress_form1 = $this->orgregistration_model->form1($org['organizationid'],$appsemid);
+		$progress_form1_advisers = $this->orgregistration_model->form1_advisers($org['organizationid'],$appsemid);
+		$progress_form2 = $this->orgregistration_model->form2($org['organizationid'],$appsemid);
+		$progress_form3_members = $this->orgregistration_model->form3_members($org['organizationid'],$appsemid);
+		$progress_form3_officers = $this->orgregistration_model->form3_officers($org['organizationid'],$appsemid);
+		$progress_form5_eventreports = $this->orgregistration_model->form5_eventreports($org['organizationid'],$appsemid);
+		$progress_form5_awards = $this->orgregistration_model->form5_awards($org['organizationid'],$appsemid);
+		$progress_form6 = $this->orgregistration_model->form6($org['organizationid'],$appsemid);
+		$progress_form7 = $this->orgregistration_model->form7($org['organizationid'],$appsemid);
+		$progress_reqs = $this->orgregistration_model->requirements($org['organizationid'],$appsemid);
 	}
 	$progress_total = $progress_form1+
 		$progress_form1_advisers+
@@ -41,45 +41,65 @@
 	<?if($this->session->user_group_is(ORG_GROUPID) && !$this->Variable->app_is_open()):?>
 		Registration is Currently Closed.
 	<?else:?>
+	<? if($this->session->user_group_is(OSA_GROUPID) && count($appsems) > 1): ?>	
+		<?= form_open('osa/form_change_appsem_submit') ?>
+		View application form for different application period:
+		<?= form_dropdown('appsem', $appsems, $appsemid) ?>
+		<?= form_hidden('orgid',$org['organizationid'])?>
+		<?= form_submit('submit', 'Go') ?>
+		<?= form_close(); ?>
+	<? endif; ?>
 	<div id="progress"></div>
 	Progress: <?=round($progress_total*10,2)?>%<?=br(1)?>
-	Status: <?=$org['orgstatusdesc']?>
+	
+	<?if($this->session->user_group_is(ORG_GROUPID)):?>
+		Status: <?=$org['orgstatusdesc']?>
+	<?else:?>
+		<?= form_open('osa/change_application_status') ?>
+			<?= form_label('Status:','orgstatus')?>
+			<?= form_dropdown('orgstatus', $statuses, $org['orgstatusid']) ?>
+			<?= form_hidden('orgid',$org['organizationid'])?>
+			<?= form_hidden('appsemid',$appsemid)?>
+			<?= form_submit('submit', 'Change') ?>
+		<?= form_close() ?>
+	<?endif;?>
+	
 	<?=br(2)?>
 	<div>
 	<? if($this->session->user_group_is(OSA_GROUPID)):?>
 		<div title= "<?=($progress_form1)?"OK":"Form1 Needs to be filled up"?>">
 		<span id="progress_form1" class="ui-icon ui-icon-<?=($progress_form1)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form1/{$aysem}/{$org['organizationid']}","Form 1 - Information Sheet");?><br />
+		<?=anchor("organization/form1/{$appsemid}/{$org['organizationid']}","Form 1 - Information Sheet");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form1_advisers)?"OK":"You need at least one confirmed adviser"?>">
 		<span id="progress_form1_advisers" class="ui-icon ui-icon-<?=($progress_form1_advisers)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form1_faculty_adviser/{$aysem}/{$org['organizationid']}","Form 1 - Faculty Advisers");?><br />
+		<?=anchor("organization/form1_faculty_adviser/{$appsemid}/{$org['organizationid']}","Form 1 - Faculty Advisers");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form2)?"OK":"You need to have an entry in the Starting Balance Field and the Collections and Disbursements Table"?>">
 		<span id="progress_form2" class="ui-icon ui-icon-<?=($progress_form2)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form2/{$aysem}/{$org['organizationid']}","Form 2 - Finance Statement");?><br />
+		<?=anchor("organization/form2/{$appsemid}/{$org['organizationid']}","Form 2 - Finance Statement");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form3_officers)?(($progress_form3_members)?"OK":"You need at least 15 confirmed members"):($progress_form3_members)?"You need at least 5 confirmed officers":"You need at least 5 confirmed officers and 15 confirmed members"?>">
 		<span id="progress_form3" class="ui-icon ui-icon-<?=($progress_form3_officers && $progress_form3_members)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form3/{$aysem}/{$org['organizationid']}","Forms 3 and 4 - Officer and Member Roster");?><br />
+		<?=anchor("organization/form3/{$appsemid}/{$org['organizationid']}","Forms 3 and 4 - Officer and Member Roster");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form5_eventreports)?(($progress_form5_awards)?"OK":"There are no Awards [Not Required]"):"All event categories should have at least one event"?>">
 		<span id="progress_form5" class="ui-icon ui-icon-<?=($progress_form5_eventreports)?(($progress_form5_awards)?"check":"alert"):"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form5/{$aysem}/{$org['organizationid']}","Form 5 - Accomplishment Report");?><br />
+		<?=anchor("organization/form5/{$appsemid}/{$org['organizationid']}","Form 5 - Accomplishment Report");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form6)?"OK":"All event categories should have at least one event"?>">
 		<span id="progress_form6" class="ui-icon ui-icon-<?=($progress_form6)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form6/{$aysem}/{$org['organizationid']}","Form 6 - Calendar of Activities");?><br />
+		<?=anchor("organization/form6/{$appsemid}/{$org['organizationid']}","Form 6 - Calendar of Activities");?><br />
 		</div>
 		
 		<div title= "<?=($progress_form7)?"OK":"Acknowledgment needed"?>">
 		<span id="progress_form7" class="ui-icon ui-icon-<?=($progress_form7)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/form7/{$aysem}/{$org['organizationid']}","Form 7 - Acknowledgment");?><br />
+		<?=anchor("organization/form7/{$appsemid}/{$org['organizationid']}","Form 7 - Acknowledgment");?><br />
 		</div>
 		
 		<div title= "<?=($progress_reqs)?"OK":"Some requirements are not yet submitted"?>">
@@ -89,7 +109,7 @@
 		
 		<div title= "<?=$org['orgstatusdesc']?>">
 		<span id="progress_reqs" class="ui-icon ui-icon-<?=($org['orgstatusid']>1)?"check":"closethick"?> progress-check-icon"></span>
-		<?=anchor("organization/submit_forms/{$aysem}/{$org['organizationid']}","Submit to OSA");?><br />
+		<?=anchor("organization/submit_forms/{$appsemid}/{$org['organizationid']}","Submit to OSA");?><br />
 		</div><br /><br />
 	<? else:?>
 		<div title= "<?=($progress_form1)?"OK":"Form1 Needs to be filled up"?>">
@@ -144,7 +164,7 @@
 	</div>
 	
 	<? if($this->session->user_group_is(OSA_GROUPID)):?>
-		<?=anchor('osa/create_clarification/'.$org['organizationid'].'/'.$aysem,'Add Message').br(2)?>
+		<?=anchor('osa/create_clarification/'.$org['organizationid'].'/'.$appsemid,'Add Message').br(2)?>
 	<? endif;?>
 	
 	<? if(count($clarifications)):?>
@@ -164,8 +184,8 @@
 			<td>
 				<?= anchor('organization/view_clarification/'.$clarification['orgclarificationid'],'View') ?> 
 				<? if($this->session->user_group_is(OSA_GROUPID)): ?>
-					<?= anchor("osa/edit_clarification/{$org['organizationid']}/{$aysem}/{$clarification['orgclarificationid']}",'Edit')?>
-					<?= anchor("osa/delete_clarification/{$clarification['orgclarificationid']}/{$org['organizationid']}/{$aysem}",'Delete')?>					
+					<?= anchor("osa/edit_clarification/{$org['organizationid']}/{$appsemid}/{$clarification['orgclarificationid']}",'Edit')?>
+					<?= anchor("osa/delete_clarification/{$clarification['orgclarificationid']}/{$org['organizationid']}/{$appsemid}",'Delete')?>					
 				<? endif;?>
 			</td>
 		</tr>
@@ -189,7 +209,7 @@
 		Search: <input name="filter" id="filter-box" value="" maxlength="30" size="30" type="text"> <input id="filter-clear-button" type="submit" value="Clear"/>
 	</div>
 	<? if($this->session->user_group_is(OSA_GROUPID)):?>
-		<?=br(2).anchor('osa/create_clarification/'.$org['organizationid'].'/'.$aysem,'Add Message')?>
+		<?=br(2).anchor('osa/create_clarification/'.$org['organizationid'].'/'.$appsemid,'Add Message')?>
 	<? endif;?>
 	<? else:?>
 		No Messages From OSA
