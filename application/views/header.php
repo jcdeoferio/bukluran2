66303b64f50ec532b31f94a15ba6e96141a6a1d6
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-06-17 13:27:06 +0000

    added message showing the requirement of javascript to be turned on.

--- application/views/header.php
+++ application/views/header.php
@@ -1,9 +1,8 @@
 <body>
-<!--
+
 <div id="jr_reject"><div id="jr_overlay" style="width: 100%; height: 100%; position: absolute; top: 0px; left: 0px; background-image: initial; background-attachment: initial; background-origin: initial; background-clip: initial; background-color: rgb(0, 0, 0); z-index: 200; opacity: 0.8; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; background-position: initial initial; background-repeat: initial initial; "></div><div id="jr_wrap" style="position: absolute; width: 100%; top: 99.25px; left: 0px; z-index: 300; text-align: center; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; "><div id="jr_inner" style="background-image: initial; background-attachment: initial; background-origin: initial; background-clip: initial; background-color: rgb(255, 255, 255); border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; border-top-style: solid; border-right-style: solid; border-bottom-style: solid; border-left-style: solid; border-top-color: rgb(204, 204, 204); border-right-color: rgb(204, 204, 204); border-bottom-color: rgb(204, 204, 204); border-left-color: rgb(204, 204, 204); font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, Verdana, sans-serif; color: rgb(79, 79, 79); margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; position: relative; height: auto; min-width: 500px; max-width: 700px; width: auto; padding-top: 20px; padding-right: 20px; padding-bottom: 20px; padding-left: 20px; font-size: 12px; background-position: initial initial; background-repeat: initial initial; "><h1 id="jr_header" style="display: block; font-size: 1.3em; color: rgb(51, 51, 51); font-family: Helvetica, Arial, sans-serif; font-weight: bold; text-align: left; padding-top: 5px; padding-right: 5px; padding-bottom: 5px; padding-left: 5px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; ">This Application Requires JavaScript to be enabled</h1><p style="text-align: left; padding-top: 5px; padding-right: 5px; padding-bottom: 5px; padding-left: 5px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; ">please enable JavaScript to continue.</p></div></div></div>
--->
 <!-- Panel -->
-<div id="toppanel">
+<div id="toppanel" style="display:none;">
 	<div id="panel">
 		<div class="content clearfix">
 			<div class="left">
@@ -80,6 +79,11 @@
 	</div>
 </div> 
 <!-- / Panel -->
+
+<script>
+	$('#jr_reject').hide();
+	$('#toppanel').show();
+</script>
 
 <div id="headerWrapper">
 <div class="container">
