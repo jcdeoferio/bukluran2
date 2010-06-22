45a86c72fd704023248782fe812791db8b18c43f
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-06-22 01:02:49 +0000

    sidebar css modifications.

--- application/views/sidebar/links_only.php
+++ application/views/sidebar/links_only.php
@@ -3,7 +3,11 @@
 		<div class='sidebar_links'>
 			<h1><?=$link['title']?></h1>
 			<?php for($i=0;$i<count($link['hrefs']);$i++): ?>
-				<?= anchor($link['hrefs'][$i],$link['anchors'][$i],($link['selected']==$i?'class="selected"':''))?>
+				<? if($link['selected']==$i):?>
+					<span class="selected"><?=$link['anchors'][$i]?></span>
+				<?else:?>
+					<?= anchor($link['hrefs'][$i],$link['anchors'][$i])?>
+				<?endif;?>
 			<?php endfor;?>
 		</div>
 	<?endforeach;?>
