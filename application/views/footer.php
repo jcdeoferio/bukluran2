2b3320525334d497b37fc59989ec921983b9f672
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-06-23 15:18:20 +0000

    footer updates.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 <div class="container">
 <div class="span-24 last">
-<center>maintained by <?=anchor('http://upprogrammingguild.org','UP Programming Guild');?></center>
+<center>Developed and Maintained by <?=anchor('http://upprogrammingguild.org','UP Programming Guild');?></center>
 </div>
 </div>
 </div>
