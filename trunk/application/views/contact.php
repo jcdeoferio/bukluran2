2f5c9a4e6d4e2222c751750ba71aa40177db6c9f
jc.deoferio@gmail.com <jc.deoferio@gmail.com@26e7e214-c618-11de-bfbd-4d0f0f27293a>
2010-06-27 09:51:56 +0000

    contact us page.

--- trunk/application/views/contact.php
+++ trunk/application/views/contact.php
@@ -1,5 +1,6 @@
 <div class="span-24 last" id="content_main">
 	<div class="contentHeader_text">
-		Contact Us
+		For Problems and Other Concerns:
 	</div>
+	<p class="grey">send an email to <?=mailto('up.bukluran@gmail.com','up.bukluran@gmail.com')?></p>
 </div>
